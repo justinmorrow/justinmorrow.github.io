ec699e8f948b421979127c6a914d5a100cf2afbf
Justin Morrow <justin.morrow81@gmail.com>
2017-12-05 22:41:14 -0500

    Got foundation working

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,43 @@
 'use strict';
 
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+const sassPaths = [
+  './node_modules/foundation-sites/scss'
+];
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-
+gulp.task('serve', ['sass', 'js'], function() {
     browserSync.init({
         server: "./"
     });
 
     gulp.watch("./scss/*.scss", ['sass']);
+    gulp.watch("./js/*.js", ['js']);
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src("./scss/*.scss")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("./css"))
-        .pipe(browserSync.stream());
+    .pipe(sass({
+      includePaths: sassPaths,
+      outputStyle: 'compressed' // if css compressed **file size**
+    })
+      .on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('js', function() {
+    return gulp.src("./node_modules/foundation-sites/dist/js/foundation.min.js")
+        .pipe(gulp.dest("js"))
 });
 
 gulp.task('default', ['serve']);
